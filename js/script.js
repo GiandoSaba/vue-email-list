af341bf4e6b744abfea5e0a52493ebeef267503b
GiandoSaba <giandosaba997@gmail.com>
2022-01-10 13:04:16 +0100

    controls

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@ const app = new Vue({
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((response) => {
-                    console.log(response.data.response);
-                    this.emailList.push(response.data.response);
+                    while(!this.emailList.includes(response.data.response)) {
+                        this.emailList.push(response.data.response);
+                    }
                 })
                 .catch((error) => {
                     console.log(error);
